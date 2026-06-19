418467c53d6e9fb372d4d670bf67d042aec59b77
alakambhavya <alakambhavya@gmail.com>
2026-06-19 11:02:21 +0530

    indexing

---
+++ indexing.py
@@ -0,0 +1,31 @@
+print("\n______________________INDEXING____________________________\n")
+data=["bhavya",1234,"i am bad girl with good attitude"]
+print(data[0])
+print(data[0][3])
+print(data[2])
+print(data[1])
+
+print("____________")
+
+name=("empowering every student to turn technology into innovation")
+print(name[1])
+
+print("_______________")
+
+std={"name":"bhanu","id":2378123,"phone_no":9032628875}
+print(std["phone_no"])
+
+print("________________")
+
+home=("chair","fridge","fan","sofa","kithcen","bottle")
+print(home[2])
+
+print("_______________")
+
+office=["employee","manager","HR","watchman","servers",("chairs","laptaps")]
+print(office[4])
+print(office[5])
+
+print("_____________")
+hotel=("dosa","idly","chapathi","parota","sambar","rice","biryani")
+print(hotel[6])
