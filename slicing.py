0ada0d5de29ac9ff7125814a301ec342cb46beea
alakambhavya <alakambhavya@gmail.com>
2026-06-20 14:58:57 +0530

    slicling

---
+++ slicing.py
@@ -0,0 +1,18 @@
+print("\n_______________________________SLICING____________________________\n")
+data="great minds discuss ideas,average minds discuss events,small minds discuss people."
+print(data[-9:-16:-1])
+print(data[20:25:1])
+
+print("_________________")
+
+name="education is the key to success in life."
+print(name[0:10:1])
+
+print("___________________") 
+
+quote="never wait for perfect moment make a moment perfect"
+print(quote[2:3:1])
+print(quote[15:22:1])
+
+
+
